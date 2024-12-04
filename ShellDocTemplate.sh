5605ad0584101d5f652b480254ced7c60d501fc9
Diego Casagranda <diego.casagranda@yahoo.com>
2024-12-04 19:20:28 -0300

    Add files via upload

---
+++ ShellDocTemplate.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+#===============================================================================
+#
+# File...........: ShellDocTemplate.sh
+# Title..........: System Test ShellScript
+# Program........: Shell Template Code -  GNU/Linux
+#
+# Description....: Basic Test
+#
+# Copyright......: Copyright(c) 2024 / @Diego.Casagranda - HackLab
+# License........: GNU GENERAL PUBLIC LICENSE - Version 3, 29 June 2007
+#
+# Author.........: Diego Casagranda
+# E-Mail.........: diego.casagranda@yahoo.com
+#
+# Dependency.....: None
+#
+# Date...........: 04/12/2024
+# Update.........: None
+#
+# Version........: 0.1.0
+#
+#===============================================================================
+#
+# ###########
+# # History #
+# ###########
+#
+#     05/06/2024 : Criação do template
+#
+#===============================================================================
